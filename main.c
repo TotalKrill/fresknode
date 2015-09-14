c7af1cb04c88f487748e3a32383ef47e03e84dbe
krille <you@example.com>
2015-09-14 17:43:43 +0200

    fulhacket is alive

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include "dw1000_ranging.h"
 #include "timer.h"
 #include "eeprom.h"
+#include "fulhacket.h"
 
 
 #include "chprintf.h"
@@ -48,8 +49,22 @@ const EXTConfig extcfg = {
 };
 
 dw1000_driver_t dw;
-
 uint16_t counter[12];
+dw1000_euid_t devid;
+
+void get_euid(void){
+    eeprom_init();
+    uint8_t uid[8] = {0,0,0,0,0,0,0,0};
+    eeprom_read(EEPROM_UID_ADDRESS, (uint8_t *)uid, 8);
+    devid.u8[0] = uid[0];
+    devid.u8[1] = uid[1];
+    devid.u8[2] = uid[2];
+    devid.u8[3] = uid[3];
+    devid.u8[4] = uid[4];
+    devid.u8[5] = uid[5];
+    devid.u8[6] = uid[6];
+    devid.u8[7] = uid[7];
+}
 
 int main(void)
 {
@@ -65,10 +80,8 @@ int main(void)
 
     // enable usb
     usbstartup();
-    eeprom_init();
-    uint8_t uid[6] = {0,0,0,0,0,0};
 
-    eeprom_read(EEPROM_UID_ADDRESS, (uint8_t *)uid, 6);
+    get_euid();
 
     //enable debug print thread
     vInitDebugPrint((BaseSequentialStream *) &SDU1);
@@ -89,94 +102,104 @@ int main(void)
         sender = true;
     }
 
-    uint8_t rxbuf[4];
     dw1000_conf_t config;
-    if(sender){
-        config.shortaddr.u16 = 0xFA77;
-    }
-    else{
-        config.shortaddr.u16 = 0xBEEF;
-        //config.shortaddr.u16 = 0xBEFF;
+    rangerRole role = get_role(devid);
+    switch(role){
+        case ANCHOR0:
+            config.shortaddr.u16 = 0;
+            break;
+        case ANCHOR1:
+            config.shortaddr.u16 = 1;
+            break;
+        case ANCHOR2:
+            config.shortaddr.u16 = 2;
+            break;
+        case ANCHOR3:
+            config.shortaddr.u16 = 3;
+            break;
+        case NODE1:
+            config.shortaddr.u16 = 4;
+            break;
+        case NODE2:
+            config.shortaddr.u16 = 5;
+            break;
     }
 
 
     dw.state = UNINITIALIZED;
-    dw1000_euid_t euid;
-    euid.u64 = 1;
 
     dw1000_generate_recommended_conf(
             &default_dw1000_hal,
             DW1000_DATARATE_850,
             DW1000_CHANNEL_2,
-            euid,
+            devid,
             &config);
 
     dw.config = &config;
-
     dw1000_init(&dw);
 
-    uint32_t zero = \
-                    DW1000_EVENT_TXFRS | \
-                    DW1000_EVENT_RXDFR | \
-                    DW1000_EVENT_AFFREJ | \
-                    DW1000_EVENT_RXFCE | \
-                    DW1000_EVENT_RXPHE | \
-                    DW1000_EVENT_LDEERR | \
-                    DW1000_EVENT_RXRFSL | \
-                    DW1000_EVENT_RXSFDTO | \
-                    DW1000_EVENT_HPDWARN | \
-                    DW1000_EVENT_TXBERR \
-                    ;
-    dw1000_set_interrupts(&default_dw1000_hal,zero);
+    /* Fulhacket */
+    start_chain_range_thd();
 
     dw1000_receive(&dw);
 
     if(sender){
         set_ranging_callback(calibration_cb);
-        //palSetPad(GPIOC, GPIOC_PIN8);
     }
     else{
-        //palClearPad(GPIOC, GPIOC_PIN8);
     }
 
     int per_loop =0;
-    uint8_t sleep =2000;
+    uint16_t sleep =2000;
     while(1)
     {
         //palTogglePad(GPIOC, GPIOC_PIN8);
         chThdSleepMilliseconds(sleep);
         dw1000_shortaddr_t dst;
-        printf("uid: 0x%2x,0x%2x, 0x%2x, 0x%2x, 0x%2x, 0x%2x \n\r",
-                uid[0],
-                uid[1],
-                uid[2],
-                uid[3],
-                uid[4],
-                uid[5]);
+        printf("euid: 0x%2x%2x%2x%2x%2x%2x%2x%2x \n\r",
+                devid.u8[0],
+                devid.u8[1],
+                devid.u8[2],
+                devid.u8[3],
+                devid.u8[4],
+                devid.u8[5],
+                devid.u8[6],
+                devid.u8[7]
+              );
 
-        if(sender){
-            dst.u16 = 0xBEEF;
-            request_ranging(&dw, dst);
-            chThdSleepMilliseconds(sleep);
-            dst.u16 =0xBEFF;
+
+        if(role == ANCHOR0){
+            dst.u16 = 0xFFFF;
             request_ranging(&dw, dst);
         }
 
         dw1000_get_event_counters(&default_dw1000_hal, counter);
-        if (per_loop == 100){
+        if (per_loop == 100 && false){   // never run
             per_loop = 0;
-           // printf("    PHR_ERRORS:    %u \n\r", counter[PHR_ERRORS]);
-           // printf("    RSD_ERRORS:    %u \n\r", counter[RSD_ERRORS]);
-           // printf("    FCS_GOOD:      %u \n\r", counter[FCS_GOOD]);
-           // printf("    FCS_ERRORS:    %u \n\r", counter[FCS_ERRORS]);
-           // printf("    FILTER_REJ:    %u \n\r", counter[FILTER_REJECTIONS]);
-           // printf("    RX_OVERRUNS:   %u \n\r", counter[RX_OVERRUNS]);
-           // printf("    SFD_TO:        %u \n\r", counter[SFD_TIMEOUTS]);
-           // printf("    PREAMBLE_TO:   %u \n\r", counter[PREAMBLE_TIMEOUTS]);
-           // printf("    RX_TIMEOUTS:   %u \n\r", counter[RX_TIMEOUTS]);
-           // printf("    TX_SENT:       %u \n\r", counter[TX_SENT]);
-           // printf("    HPWARN:        %u \n\r", counter[HALF_PERIOD_WARNINGS]);
-           // printf("    TX_PWRUP_WARN: %u \n\r", counter[TX_PWRUP_WARNINGS]);
+             printf("    PHR_ERRORS:    %u \n\r",
+                     counter[PHR_ERRORS]);
+             printf("    RSD_ERRORS:    %u \n\r",
+                     counter[RSD_ERRORS]);
+             printf("    FCS_GOOD:      %u \n\r",
+                     counter[FCS_GOOD]);
+             printf("    FCS_ERRORS:    %u \n\r",
+                     counter[FCS_ERRORS]);
+             printf("    FILTER_REJ:    %u \n\r",
+                     counter[FILTER_REJECTIONS]);
+             printf("    RX_OVERRUNS:   %u \n\r",
+                     counter[RX_OVERRUNS]);
+             printf("    SFD_TO:        %u \n\r",
+                     counter[SFD_TIMEOUTS]);
+             printf("    PREAMBLE_TO:   %u \n\r",
+                     counter[PREAMBLE_TIMEOUTS]);
+             printf("    RX_TIMEOUTS:   %u \n\r",
+                     counter[RX_TIMEOUTS]);
+             printf("    TX_SENT:       %u \n\r",
+                     counter[TX_SENT]);
+             printf("    HPWARN:        %u \n\r",
+                     counter[HALF_PERIOD_WARNINGS]);
+             printf("    TX_PWRUP_WARN: %u \n\r",
+                     counter[TX_PWRUP_WARNINGS]);
         }
         per_loop++;
 
